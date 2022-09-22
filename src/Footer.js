487893b4b390beaccf06caf029e2230a09fec4c0
Mathieu Roberts <mathieulroberts@gmail.com>
2022-09-21 18:33:12 -0700

    finished lab01

---
+++ src/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Footer extends React.Component {
+    render() {
+        return(
+            <>
+            <p>Mathieu Roberts made this page</p>
+            </>
+        )
+    }
+}
+
+
+
+
+
+
+
+export default Footer;
