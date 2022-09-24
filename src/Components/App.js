d15f7d2a42d9a961af668b5924631ac4629bf3db
Mathieu Roberts <mathieulroberts@gmail.com>
2022-09-23 22:22:50 -0700

    lab03 done

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+
 import '../Component-style/CssReset.css';
 import '../Component-style/App.css';
+
 import Header from './Header.js'
 import Main from './Main.js'
 import Footer from './Footer.js';
+
+import '../custom.scss'
 
 
 
