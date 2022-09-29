ed632a26c39baf96e264ede9080484d6262d03da
Mathieu Roberts <mathieulroberts@gmail.com>
2022-09-28 19:34:52 -0700

    form done

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,23 +3,26 @@ import React from 'react';
 import '../Component-style/CssReset.css';
 import '../Component-style/App.css';
 
+import '../custom.scss'
+
 import Header from './Header.js'
 import Main from './Main.js'
 import Footer from './Footer.js';
 
-import '../custom.scss'
-
-
-
+import { Container } from 'react-bootstrap';
 
 
 class App extends React.Component {
   render() {
     return(
       <>
+      <Container>
         <Header/>
+      </Container>
         <Main/>
+        <Container>
         <Footer/>
+        </Container>
       </>
     );
   }
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,23 +1,48 @@
 import React from "react";
+import Form from 'react-bootstrap/Form';
 import HornedBeast from './HornedBeast.js';
 import {arr} from './data.js';
+import { Container } from 'react-bootstrap';
 
 
 class Main extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            hornNum: this.props.hornNum,
+        };
+    };
+
     render() {
         return(
             <>
                 <h1>Click an image to favorite it!</h1>
-                {arr.map(x => {
-                    return(
-                        <HornedBeast key={x._id} image_url={x.image_url} title={x.title} description={x.description} />
-                    )
-                })}
+                <Container>
+                    <>
+                        <Form>
+                            <Form.Group className="mb-3" controlId="hornNum">
+                                <Form.Label>Order by Horns</Form.Label>
+                                <Form.Control as="select" onChange={e => {this.setState({hornNum: Number(e.target.value)})}}>
+                                    <option value='1'>1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="100">100</option>
+                                </Form.Control>
+                            </Form.Group>
+                        </Form>
+                    </>
+                </Container>
+                <Container>
+                    {arr.filter(x => {
+                        return x.horns === this.state.hornNum;
+                        }).map(x => {
+                        return <HornedBeast key={x._id} image_url={x.image_url} title={x.title} description={x.description} horns={x.horns} />
+                    })}
+                </Container>
             </>
         )
     }
 }
-
 
 
 
